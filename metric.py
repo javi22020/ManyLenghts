6f87fabe0d674f2c8662a9d2ac57ca24611a44ab
javi22020 <90896750+javi22020@users.noreply.github.com>
2024-01-09 08:42:41 +0100

    first commit

---
+++ metric.py
@@ -0,0 +1,275 @@
+def meter_to_millimeter(meter: float) -> float:
+    """
+    Convert meter to millimeter.
+    """
+    return meter * 1000
+
+def millimeter_to_meter(millimeter: float) -> float:
+    """
+    Convert millimeter to meter.
+    """
+    return millimeter / 1000
+
+def meter_to_decimeter(meter: float) -> float:
+    """
+    Convert meter to decimeter.
+    """
+    return meter * 10
+
+def decimeter_to_meter(decimeter: float) -> float:
+    """
+    Convert decimeter to meter.
+    """
+    return decimeter / 10
+
+def meter_to_decameter(meter: float) -> float:
+    """
+    Convert meter to decameter.
+    """
+    return meter / 10
+
+def decameter_to_meter(decameter: float) -> float:
+    """
+    Convert decameter to meter.
+    """
+    return decameter * 10
+
+def meter_to_centimeter(meter: float) -> float:
+    """
+    Convert meter to centimeter.
+    """
+    return meter * 100
+
+def centimeter_to_meter(centimeter: float) -> float:
+    """
+    Convert centimeter to meter.
+    """
+    return centimeter / 100
+
+def meter_to_kilometer(meter: float) -> float:
+    """
+    Convert meter to kilometer.
+    """
+    return meter / 1000
+
+def kilometer_to_meter(kilometer: float) -> float:
+    """
+    Convert kilometer to meter.
+    """
+    return kilometer * 1000
+
+def meter_to_inch(meter: float) -> float:
+    """
+    Convert meter to inch.
+    """
+    return meter * 39.3701
+
+def inch_to_meter(inch: float) -> float:
+    """
+    Convert inch to meter.
+    """
+    return inch / 39.3701
+
+def meter_to_foot(meter: float) -> float:
+    """
+    Convert meter to foot.
+    """
+    return meter * 3.28084
+
+def foot_to_meter(foot: float) -> float:
+    """
+    Convert foot to meter.
+    """
+    return foot / 3.28084
+
+def meter_to_yard(meter: float) -> float:
+    """
+    Convert meter to yard.
+    """
+    return meter * 1.09361
+
+def yard_to_meter(yard: float) -> float:
+    """
+    Convert yard to meter.
+    """
+    return yard / 1.09361
+
+def meter_to_mile(meter: float) -> float:
+    """
+    Convert meter to mile.
+    """
+    return meter * 0.000621371
+
+def mile_to_meter(mile: float) -> float:
+    """
+    Convert mile to meter.
+    """
+    return mile / 0.000621371
+
+def meter_to_nautical_mile(meter: float) -> float:
+    """
+    Convert meter to nautical mile.
+    """
+    return meter * 0.000539957
+
+def nautical_mile_to_meter(nautical_mile: float) -> float:
+    """
+    Convert nautical mile to meter.
+    """
+    return nautical_mile / 0.000539957
+
+def meter_to_fathom(meter: float) -> float:
+    """
+    Convert meter to fathom.
+    """
+    return meter * 0.546807
+
+def fathom_to_meter(fathom: float) -> float:
+    """
+    Convert fathom to meter.
+    """
+    return fathom / 0.546807
+
+def meter_to_furlong(meter: float) -> float:
+    """
+    Convert meter to furlong.
+    """
+    return meter * 0.00497096
+
+def furlong_to_meter(furlong: float) -> float:
+    """
+    Convert furlong to meter.
+    """
+    return furlong / 0.00497096
+
+def meter_to_parsec(meter: float) -> float:
+    """
+    Convert meter to parsec.
+    """
+    return meter * 3.24078e-17
+
+def parsec_to_meter(parsec: float) -> float:
+    """
+    Convert parsec to meter.
+    """
+    return parsec / 3.24078e-17
+
+def meter_to_astronomical_unit(meter: float) -> float:
+    """
+    Convert meter to astronomical unit.
+    """
+    return meter * 6.68459e-12
+
+def astronomical_unit_to_meter(astronomical_unit: float) -> float:
+    """
+    Convert astronomical unit to meter.
+    """
+    return astronomical_unit / 6.68459e-12
+
+def meter_to_light_year(meter: float) -> float:
+    """
+    Convert meter to light year.
+    """
+    return meter * 1.057e-16
+
+def light_year_to_meter(light_year: float) -> float:
+    """
+    Convert light year to meter.
+    """
+    return light_year / 1.057e-16
+
+def meter_to_rod(meter: float) -> float:
+    """
+    Convert meter to rod.
+    """
+    return meter * 0.19884
+
+def rod_to_meter(rod: float) -> float:
+    """
+    Convert rod to meter.
+    """
+    return rod / 0.19884
+
+def meter_to_chain(meter: float) -> float:
+    """
+    Convert meter to chain.
+    """
+    return meter * 0.0497097
+
+def chain_to_meter(chain: float) -> float:
+    """
+    Convert chain to meter.
+    """
+    return chain / 0.0497097
+
+def meter_to_angstrom(meter: float) -> float:
+    """
+    Convert meter to angstrom.
+    """
+    return meter * 1e+10
+
+def angstrom_to_meter(angstrom: float) -> float:
+    """
+    Convert angstrom to meter.
+    """
+    return angstrom / 1e+10
+
+def meter_to_micron(meter: float) -> float:
+    """
+    Convert meter to micron.
+    """
+    return meter * 1000000
+
+def micron_to_meter(micron: float) -> float:
+    """
+    Convert micron to meter.
+    """
+    return micron / 1000000
+
+def meter_to_nanometer(meter: float) -> float:
+    """
+    Convert meter to nanometer.
+    """
+    return meter * 1000000000
+
+def nanometer_to_meter(nanometer: float) -> float:
+    """
+    Convert nanometer to meter.
+    """
+    return nanometer / 1000000000
+
+def meter_to_picometer(meter: float) -> float:
+    """
+    Convert meter to picometer.
+    """
+    return meter * 1000000000000
+
+def picometer_to_meter(picometer: float) -> float:
+    """
+    Convert picometer to meter.
+    """
+    return picometer / 1000000000000
+
+def meter_to_femtometer(meter: float) -> float:
+    """
+    Convert meter to femtometer.
+    """
+    return meter * 1000000000000000
+
+def femtometer_to_meter(femtometer: float) -> float:
+    """
+    Convert femtometer to meter.
+    """
+    return femtometer / 1000000000000000
+
+def meter_to_attometer(meter: float) -> float:
+    """
+    Convert meter to attometer.
+    """
+    return meter * 1e+18
+
+def attometer_to_meter(attometer: float) -> float:
+    """
+    Convert attometer to meter.
+    """
+    return attometer / 1e+18
